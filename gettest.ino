c500758c9d7b822e5cea0af2c64c4e9e83287fdd
Wasiq-Shibly <55593937+Wasiq-Shibly@users.noreply.github.com>
2022-01-21 19:23:52 +0600

    code change

--- gettest.ino
+++ gettest.ino
@@ -3,56 +3,52 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-const char* ssid = "jimmy2";
-const char* password = "neways3747";
+const char *ssid = "jimmy2";
+const char *password = "neways3747";
 
-//Your Domain name with URL path or IP address with path
-const char* serverName = "http://10.100.93.58:8080/water/waterqt.php?";
+String serverName = "http://10.100.93.58:8080/water/waterqt.php";
 
 unsigned long lastTime = 0;
 
-unsigned long timerDelay = 5000;
+unsigned long timerDelay = 1000;
 
-void setup() {
+float temperature = 25.26, tdsValue = 185;
+
+void setup()
+{
   Serial.begin(115200);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
- 
+
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-void loop() 
-
- 
- {
- 
-    if(WiFi.status()== WL_CONNECTED){
-
-
-      
-      WiFiClient client;
-      HTTPClient http;
-      http.begin(client, serverName);
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      String httpRequestData = "temperature=24.25&tdsValue=500.85";           
-      int httpResponseCode = http.POST(httpRequestData);
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println(payload);
-      http.end();
-      
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-
+void loop()
+{
+  Serial.println("database");
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    WiFiClient client;
+    HTTPClient http;
+    http.useHTTP10(true);
+    http.begin(client, serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String httpRequestData = "temperature=";
+    httpRequestData += temperature;
+    httpRequestData += "&tdsValue=";
+    httpRequestData += tdsValue;
+    Serial.println(httpRequestData);
+    http.addHeader("authorization", "api_pi_12345678!@#$%^&*");
+    int httpResponseCode = http.POST(httpRequestData);
+    Serial.println(httpResponseCode);
+  }
 }
